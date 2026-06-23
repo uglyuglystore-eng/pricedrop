5a0dcff1bcd6a31e2ea0d10ff3fd26c00110b542
uglyuglystore-eng <uglyuglystore@gmail.com>
2026-06-23 09:44:07 -0400

    Create _app.js

---
+++ pages/_app.js
@@ -0,0 +1,3 @@
+import'../styles/globals.css'
+import Head from'next/head'
+export default function App({Component,pageProps}){return(<><Head><meta name="viewport" content="width=device-width,initial-scale=1"/><title>Price Drop</title></Head><Component {...pageProps}/></>)}
